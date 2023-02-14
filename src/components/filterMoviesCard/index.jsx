3f14b34349ed9a2783af36a71e7cbf53a0abc6bc
Pawel Paszki <pawelpaszki@gmail.com>
2023-02-14 11:33:00 +0000

    Refactor: Template list page; HTTP API calls centralized.

--- src/components/filterMoviesCard/index.jsx
+++ src/components/filterMoviesCard/index.jsx
@@ -11,6 +11,8 @@ import SortIcon from '@mui/icons-material/Sort';
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
+import { getGenres } from "../../api/tmdb-api";
+
 const styles = {
   root: {
     maxWidth: 345,
@@ -28,20 +30,11 @@ export default function FilterMoviesCard(props) {
   const [genres, setGenres] = useState([{ id: '0', name: "All" }])
 
   useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-      import.meta.env.VITE_TMDB_KEY
-    )
-      .then(res => res.json())
-      .then(json => {
-        // console.log(json.genres) 
-        return json.genres
-      })
-      .then(apiGenres => {
-        setGenres([genres[0], ...apiGenres]);
-      });
+    getGenres().then((allGenres) => {
+      setGenres([genres[0], ...allGenres]);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   const handleChange = (e, type, value) => {
     e.preventDefault()
