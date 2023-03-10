1899f62b61597275d6661cbe137d98a14ecc3ada
Pawel Paszki <pawelpaszki@gmail.com>
2023-03-10 16:43:26 +0000

    Add new review support.

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -12,7 +12,6 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
 import img from '../../images/film-poster-placeholder.png'
 import { MoviesContext } from "../../contexts/moviesContext";
 
@@ -33,11 +32,6 @@ export default function MovieCard({ movie, action }) {
   } else {
     movie.favourite = false
   }
-
-  const handleAddToFavourite = (e) => {
-    e.preventDefault();
-    addToFavourites(movie);
-  };
 
   return (
     <Card sx={styles.card}>
--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@ import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,6 +32,7 @@ const App = () => {
             <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
+            <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
             <Route path="/" element={<HomePage />} />
             <Route path="/upcoming" element={<UpcomingMoviesPage />} />
             <Route path="*" element={<Navigate to="/" />} />
