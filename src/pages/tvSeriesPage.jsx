f87f82ae948b1d68b578144a5b543200733362d5
Pawel Paszki <pawelpaszki@gmail.com>
2023-03-16 21:54:33 +0000

    chore: cleanup

--- src/pages/tvSeriesPage.jsx
+++ src/pages/tvSeriesPage.jsx
@@ -3,7 +3,7 @@ import PageTemplate from "../components/templateTvSeriesListPage";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import { getSeries } from "../api/tmdb-api";
-import AddToFavouriteTvSeriesIcon from '../components/cardIcons/AddToFavouriteTvSeries';
+import AddToFavouriteTvSeriesIcon from "../components/cardIcons/AddToFavouriteTvSeries";
 
 const TvSeriesPage = (props) => {
   const { data, error, isLoading, isError } = useQuery("discoverTv", getSeries);
@@ -23,7 +23,6 @@ const TvSeriesPage = (props) => {
       <PageTemplate
         title="Discover Tv series"
         tvSeries={tvSeries}
-        // action={null}
         action={(movie) => {
           return <AddToFavouriteTvSeriesIcon movie={movie} />
         }}
