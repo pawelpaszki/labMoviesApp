1cee422013d3101cee7221312dc351ef46917f83
Pawel Paszki <pawelpaszki@gmail.com>
2023-03-19 10:41:12 +0000

    fix: import

--- src/pages/tvSeriesPage.jsx
+++ src/pages/tvSeriesPage.jsx
@@ -3,7 +3,7 @@ import PageTemplate from "../components/templateTvSeriesListPage";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import { getSeries } from "../api/tmdb-api";
-import AddToFavouriteTvSeriesIcon from "../components/cardIcons/AddToFavouriteTvSeries";
+import AddToFavouriteTvSeriesIcon from "../components/cardIcons/addToFavouriteTvSeries";
 
 const TvSeriesPage = (props) => {
   const { data, error, isLoading, isError } = useQuery("discoverTv", getSeries);
