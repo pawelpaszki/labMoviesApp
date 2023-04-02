2bd609ae3a1b3e3b6ce640da3293e3814728da41
Pawel Paszki <pawelpaszki@gmail.com>
2023-04-02 18:27:30 +0100

    feat: add create playlist form to storybook support

---
+++ src/stories/createPlaylsit.stories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CreatePlaylistPage from "../pages/createPlaylistPage";
+import { MemoryRouter } from "react-router";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default {
+  title: "Create playlist/create Playlist Form form",
+  component: CreatePlaylistPage,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    ),
+  ],
+};
+
+export const Basic = () => <CreatePlaylistPage />;
+
